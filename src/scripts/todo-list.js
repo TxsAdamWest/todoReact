0b8525181010621290675a6c908403f080e56d8d
TxsAdamWest <Twinadanz@gmail.com>
2016-10-17 23:26:13 -0500

    added keyframe animation styles

--- src/scripts/todo-list.js
+++ src/scripts/todo-list.js
@@ -75,11 +75,12 @@ export const TodoListView = React.createClass({
 //Components
  const Nav = React.createClass({
 	render: function(){
+
 		return(
 			<nav className="navbar navbar-inverse navbar-fixed-top">
 			     <div className="container">
 			        <div className="navbar-header">
-			          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+			          <button type="button" className="navbar-toggle collapsed" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
 			            <span className="sr-only">Toggle navigation</span>
 			            <span className="icon-bar"></span>
 			            <span className="icon-bar"></span>
